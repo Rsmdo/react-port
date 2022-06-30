c18a9218862b89211d65ae6762e660723d7d4fc9
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-06-30 17:39:23 -0400

    changes to file structure and implenation of bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Header from './Components/Header'; 
-import Nav from './Components/Nav';
 import Project from './Components/Project';
 import About from './Components/About';
 import Contact from './Components/Contact';
@@ -8,33 +7,32 @@ import Resume from './Components/Resume';
 import Footer from './Components/Footer';
 
 function App() {
-  const [activePage, setActivePage] = useState({
-    About: true,
-    Project: false,
-    Contact: false,
-    Resume: false,
-  });
+  const [categories] = useState(['about', 'portfolio', 'contact', 'resume']);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  function renderComponent(currentCategory) {
+    switch (currentCategory) {
+      case 'about':
+        return <About />;
+      case 'portfolio':
+        return <Project />;
+      case 'contact':
+        return <Contact />;
+      case 'resume':
+        return <Resume />; 
+      default:
+        return <About />;
+    }
+  }
   return (
-    <main>
-      <nav>
-        <Header />
-        <Nav activePage={activePage} setActivePage={setActivePage} />
-      </nav>
-      <section >
-        {activePage.about ? (
-          <About />
-        ) : activePage.portfolio ? (
-          <Project />
-        ) : activePage.contact ? (
-          <Contact />
-        ) : activePage.resume ? (
-          <Resume />
-        ) : (
-          ''
-        )}
-      </section>
+    <div className="App min-vh-100 position-relative">
+      <div className='content-wrap'>
+        <Header currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
+        <main>
+          {renderComponent(currentCategory)}
+        </main>
+      </div>
       <Footer />
-    </main>
+    </div>
   );
 }
 
