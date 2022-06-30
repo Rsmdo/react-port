7f62d31fd3668773db6dfdd9d8bed5ef4743b64c
Royden Semedo <roydensemedo@Roydens-MacBook-Pro.local>
2022-06-30 18:59:44 -0400

    added header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Nav from '../Nav';
+import snow from '../../images/header-background.jpg';
 
-const Header = () => {
-  return (
-      <div>
-          HEader
-      </div>
-  )
+function Header(props) {
+    const { currentCategory, setCurrentCategory } = props;
+    return (
+        <header className='d-flex flex-column flex-lg-row justify-content-center justify-content-lg-end p-2' style={{backgroundImage: `url(${snow})`, backgroundSize: 'cover'}}>
+            <Nav currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
+        </header>
+    );
 }
-
 
 export default Header;
